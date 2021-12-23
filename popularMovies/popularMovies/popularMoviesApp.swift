2cebe443c77a13adad983894d1ca98262ae0d579
Íris Soares <iss3@cin.ufpe.br>
2021-12-23 17:07:40 -0300

    create xcode project

---
+++ popularMovies/popularMovies/popularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  popularMoviesApp.swift
+//  popularMovies
+//
+//  Created by iris on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct popularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
