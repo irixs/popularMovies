d86359da6b402aa14632e7afae59d65e01f0028b
Íris Soares <iss3@cin.ufpe.br>
2021-12-25 19:32:24 -0300

    create movie cell view

---
+++ popularMovies/popularMovies/Views/MovieCellView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCellView.swift
+//  popularMovies
+//
+//  Created by iris on 25/12/21.
+//
+
+import SwiftUI
+
+struct MovieCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieCellView()
+    }
+}
